ca30d143d712131e2bb488e2f1d328ca08f3c317
feder <feder@host.docker.internal>
2025-12-07 15:39:47 +0100

    add controller skeleton

--- fabrickAPI/src/main/java/it/project/application/Application.java
+++ fabrickAPI/src/main/java/it/project/application/Application.java
@@ -2,8 +2,10 @@ package it.project.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("it.project")
 public class Application {
 
 	public static void main(String[] args) {
---
+++ fabrickAPI/src/main/java/it/project/controller/Controller.java
@@ -0,0 +1,45 @@
+package it.project.controller;
+
+import java.time.LocalDate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+	
+	@GetMapping("/lettura_saldo")
+	public ResponseEntity<String> bankingAccountCash(@RequestParam String idAccount) {
+		logger.info("Sto cercando le informazioni per {}",idAccount);
+		//TODO
+		return ResponseEntity.ok("Lettura saldo effettuata");
+	}
+	
+	@GetMapping("/lista_transizioni")
+	public ResponseEntity<String> listTransaction(@RequestParam String idAccount, 
+								@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateStart, 
+								@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateEnd) {
+		logger.info("Dammi la lista transizioni per questo account {} in questo intervallo di tempo {}:{}",idAccount,dateStart,dateEnd);
+		//TODO
+		return ResponseEntity.ok("lettura lista transazioni");
+	}
+	
+	@PostMapping("/bonifico")
+	public ResponseEntity<String> bankTranser() {
+		logger.info("Devo fare un bonifico");
+		//TODO
+		return ResponseEntity.ok("bonifico effettuato");
+	}
+	
+	
+	
+	
+}
